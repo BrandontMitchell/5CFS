7290ba016ede2f9b6446790e2f8dd8c70f43677a
BrandontMitchell <btmbaseball99@gmail.com>
2019-10-03 21:58:37 -0700

    import flask_sqlalch and init proj_dir

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy 
 import os, sys
+
+
+proj_dir = os.path.dirname(os.path.abspath(__file__))
+db_file = ""
 
 app = Flask(__name__)
 
